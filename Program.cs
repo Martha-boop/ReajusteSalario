bf29bdcdbeb21ff4be74b74cee41464643f46745
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2021-03-29 20:48:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReajusteSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 0;
+            double novosalario = 0;
+
+            Console.WriteLine("Efetue o reajuste do sálarial!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Informe o sálario:");
+                salario = int.Parse(Console.ReadLine());
+                if(salario <= 300)
+                { // 50 %
+                    novosalario = salario * 1.50;
+
+                }
+                else
+                { // 30%
+                    novosalario = salario + ((salario * 30) / 100);
+
+                }
+                Console.WriteLine("Salario reajustado:" + novosalario);
+            }
+            Console.ReadLine();
+        }
+    }
+}
